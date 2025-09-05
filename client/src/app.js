edce0c1665ba36f79e188b04052f100fcae1a0ba
jmegabassd <jmegabassd@gmail.com>
2025-09-05 12:27:51 +0100

    task: add async function to recieve guest data

--- client/src/app.js
+++ client/src/app.js
@@ -1,19 +1,13 @@
 console.log("MOO!");
 
-//select form from DOM
 const commentsForm = document.getElementById("form-comments");
-// add submit event
 commentsForm.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
-  //prevent the data from going to the url
   event.preventDefault();
-  //create a new template object using my form inputs
   const formData = new FormData(commentsForm);
-  //fill the input data in the template object
   const formValues = Object.fromEntries(formData);
   console.log(formValues);
-  //send data to the server
   fetch("http://localhost:7777/comments-add", {
     method: "POST",
     headers: {
@@ -22,3 +16,11 @@ function handleSubmit(event) {
     body: JSON.stringify({ formValues }),
   });
 }
+
+async function getGuestMessages() {
+  const response = await fetch("http://localhost:7777/comments");
+  console.log("HTTP Response:", response);
+  const json = await response.json();
+  console.log("JSON Data:", json);
+}
+getGuestMessages();
