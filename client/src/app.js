f38444b3d538f8e954a83d24f224499daa184b8c
jmegabassd <jmegabassd@gmail.com>
2025-09-06 09:23:33 +0100

    task: format date for user, added click noise to submit button

--- client/src/app.js
+++ client/src/app.js
@@ -1,7 +1,9 @@
 console.log("MOO!");
 
+const audio = new Audio("./src/audio/click.mp3");
 const commentsForm = document.getElementById("form-comments");
 const thanksForm = document.getElementById("hidden-thanks");
+
 commentsForm.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
@@ -17,6 +19,7 @@ function handleSubmit(event) {
   });
   commentsForm.style.display = "none";
   thanksForm.style.display = "block";
+  audio.play();
 }
 
 async function getGuestMessages() {
@@ -26,9 +29,12 @@ async function getGuestMessages() {
   const messagecontainer = document.getElementById("post-submit");
   messagecontainer.innerHTML = "";
   json.forEach((item) => {
+    const visitDate = new Date(item.visitdate);
+    const formattedDate = visitDate.toLocaleDateString();
+    const formattedTime = visitDate.toLocaleTimeString();
     const messageDiv = document.createElement("div");
     messageDiv.classList.add("comment");
-    messageDiv.textContent = `${item.name}\n${item.visitdate}\n${item.comments}`;
+    messageDiv.textContent = `${item.name}\n${formattedDate} at ${formattedTime}\n${item.comments}`;
     messagecontainer.appendChild(messageDiv);
   });
 }
